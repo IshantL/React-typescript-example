4102837b82b0e6f2264af5db0d197610ab0282a5
Ishant Lambhate <ishant.lambhate@synapse.com>
2019-07-25 17:54:01 +0530

    done with webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom';
 
+const sum = (a: number, b: number):number => {
+  return a+b;
+}
 export default function App(): JSX.Element {
   return (
     <h1>
-      Hello!!
+      Hello!! {sum(10,5)}
     </h1>
   )
 }
