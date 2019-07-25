a4fc77e6b39791e7257d10231ed98e496deb21a4
Ishant Lambhate <ishant.lambhate@synapse.com>
2019-07-25 23:01:38 +0530

    hooks usestate

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import ReactDom from 'react-dom';
+import React, { Fragment, useState } from "react";
+import ReactDom from "react-dom";
+import { strict } from "assert";
+import { string } from "prop-types";
 
-const sum = (a: number, b: number):number => {
-  return a+b;
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+//type references another type
+
+interface Itodo {
+  text:string
+  complete: boolean
 }
+//interface creates new tpes
 export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<Itodo[]>([]);
+  
+  const handleSubmit = (e:FormElem):void =>{
+    e.preventDefault();
+    addTodo(value);
+    setValue('');
+  }
+  const addTodo =(text:string) =>{
+    const newTodos:Itodo[] = [...todos, { text, complete:false}]
+    setTodos(newTodos);
+  }
   return (
-    <h1>
-      Hello!! {sum(10,5)}
-    </h1>
-  )
+    <Fragment>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required/>
+        <button type="submit">Add Todo</button>
+      </form>
+    </Fragment>
+  );
 }
-const root = document.getElementById('app-root');
+const root = document.getElementById("app-root");
 
 ReactDom.render(<App />, root);
